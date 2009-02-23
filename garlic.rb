9786749817e13b2be9405a778d2b6038a3cec64d
Ian White <ian.w.white@gmail.com>
2009-02-23 19:05:55 +0000

    Add garlic CI definition

---
+++ garlic.rb
@@ -0,0 +1,30 @@
+garlic do
+  # this plugin
+  repo "git_friendly_dumper", :path => '.'
+  
+  # other repos
+  repo "rails", :url => "git://github.com/rails/rails"
+  repo "rspec", :url => "git://github.com/dchelimsky/rspec"
+  repo "rspec-rails", :url => "git://github.com/dchelimsky/rspec-rails"
+  
+  # target railses
+  ['origin/master', 'origin/2-2-stable', 'origin/2-1-stable', 'origin/2-0-stable'].each do |rails|
+    
+    # declare how to prepare, and run each CI target
+    target "Rails: #{rails}", :tree_ish => rails do
+      prepare do
+        plugin "git_friendly_dumper", :clone => true # so we can work in targets
+        plugin "rspec"
+        plugin "rspec-rails" do
+          `script/generate rspec -f`
+        end
+      end
+    
+      run do
+        cd "vendor/plugins/git_friendly_dumper" do
+          sh "rake rcov:verify"
+        end
+      end
+    end
+  end
+end
