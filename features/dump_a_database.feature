dffcb2bd69abf5e3e8fb17c6363bc45d95921aaa
Ian White <ian.w.white@gmail.com>
2010-07-14 17:05:38 +0100

    Example of table diffing, and testing that our db steps work

--- features/dump_a_database.feature
+++ features/dump_a_database.feature
@@ -4,9 +4,19 @@ Feature: Dump a database
     Given I am in an empty app
     Given a Rakefile exists which has an environment task and loads git_friendly_dumper tasks
     And an empty database
-    And the database has a users table
-    And the users table has some data
+    And the database has a "users" table (with timestamps):
+     | name (string) | surname (string) |
+     | Fred          | Bloggs           |
+     | Ethel         | Smith            |
+     | Jane          | Heidie           |
     
+  Scenario: test step works
+    Then the "users" table should consist of:
+     | name  | surname |
+     | Fred  | Bloggs  |
+     | Ethel | Smith   |
+     | Jane  | Heidie  |
+  
   Scenario: rake db:dump FORCE=true
     When I successfully run "rake db:dump"
     Then the output should contain "Dumping data and structure from database to db/dump"
--- features/step_definitions/database_steps.rb
+++ features/step_definitions/database_steps.rb
@@ -10,17 +10,66 @@ Given /^an empty database$/ do
   end
 end
 
-Given /^the database has a users table$/ do
-  ActiveRecord::Base.connection.create_table :users, :force => true do |t|
-    t.string :name
-    t.timestamps
+Then /^show me the "([^"]*)" table$/ do |table_name|
+  puts "<pre>#{table_contents(table_name, false).to_yaml}</pre>"
+end
+
+Given /^the database has a "([^"]*)" table( \(with timestamps\))?:$/ do |table_name, timestamps, table|
+  create_table(table_name)
+  add_timestamps_to_table(table_name) if timestamps
+  
+  columns = []
+  table.headers.each do |column_def|
+    raise "column_def should look like 'column_name (type)'" unless match = column_def.match(/(\w+) \((\w+)\)/)
+    add_column_to_table(table_name, match[1], match[2])
+    columns << match[1]
+  end
+  
+  table.rows.each do |row|
+    attrs = {}
+    columns.each_with_index do |column_name, index|
+      attrs[column_name] = row[index]
+    end
+    insert_record_into_table(table_name, attrs)
   end
 end
 
-Given /^the users table has some data$/ do
-  user_class = Class.new(ActiveRecord::Base)
-  user_class.table_name = 'users'
-  user_class.create! :name => "Fred"
-  user_class.create! :name => "Ethel"
-  user_class.create! :name => "James"
+Then /^the "([^"]*)" table should consist of:$/ do |table_name, table|
+  table.diff!(table_contents(table_name))
 end
+
+module DatabaseHelpers
+  def create_table(name)
+    ActiveRecord::Base.connection.create_table name do
+    end
+  end
+
+  def add_timestamps_to_table(name)
+    ActiveRecord::Base.connection.change_table name do |t|
+      t.timestamps
+    end
+  end
+
+  def add_column_to_table(table_name, column_name, type)
+    ActiveRecord::Base.connection.change_table table_name do |t|
+      t.send type, column_name
+    end
+  end
+
+  def class_for_table(table_name)
+    @class_for_table ||= {}
+    @class_for_table[table_name] ||= begin
+      Class.new(ActiveRecord::Base).tap {|klass| klass.table_name = table_name }
+    end
+  end
+
+  def insert_record_into_table(table_name, attrs)
+    class_for_table(table_name).create! attrs
+  end
+
+  def table_contents(table_name)
+    contents = class_for_table(table_name).all.map(&:attributes)
+  end
+end
+
+World(DatabaseHelpers)
